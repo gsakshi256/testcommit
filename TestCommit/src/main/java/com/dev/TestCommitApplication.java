bdba96774848eb68243cb0e57531bfc561f6573b
gsakshi256 <36441670+gsakshi256@users.noreply.github.com>
2018-02-14 15:36:22 +0530

    intial commit

---
+++ TestCommit/src/main/java/com/dev/TestCommitApplication.java
@@ -0,0 +1,12 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCommitApplication.class, args);
+	}
+}
